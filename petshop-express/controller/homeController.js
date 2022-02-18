a01a592f83ce7bd5fe10b627aebb106d06a5382e
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2022-02-18 20:27:21 -0300

    deixando a página home dinamica

--- petshop-express/controller/homeController.js
+++ petshop-express/controller/homeController.js
@@ -1,6 +1,8 @@
+var servicosModel = require("../model/servico.json")
+
 const homeController = {
     home: (req,res) =>{
-        res.render('index')
+        res.render('index',{listaServicos: servicosModel})
     }
 }
 
